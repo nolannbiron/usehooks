c61ea6f63cf0a216443e9269f97ed0fefbdea8e5
Nolann Biron <biron.nolann@gmail.com>
2022-03-29 18:13:46 +0200

    add useCallback on useBoolean

--- src/hooks/utils/useBoolean.ts
+++ src/hooks/utils/useBoolean.ts
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const useBoolean = (initialValue = false) => {
   const [value, setValue] = React.useState(initialValue);
 
-  const setTrue = () => setValue(true);
-  const setFalse = () => setValue(false);
-  const toggle = () => setValue(!value);
+  const setTrue = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  const toggle = useCallback(() => setValue(!value), []);
 
   return { value, setTrue, setFalse, toggle };
 };
