7a40d2b6afe83aec42911b601901edce25687a12
Nolann Biron <biron.nolann@gmail.com>
2022-03-30 11:04:48 +0200

    add useCopyToClipboard

---
+++ src/_tests_/useCopyToClipboard.test.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useSessionStorage } from "../hooks/storage";
+import { useCopyToClipboard } from "../hooks/utils";
+
+test("should increment counter", () => {
+  const { result } = renderHook(() => useCopyToClipboard());
+
+  act(() => {
+    result.current[1]('John')
+  });
+
+  expect(result.current[0]).toBe("John");
+  expect(navigator.clipboard.readText()).toBe("John");
+});
--- src/hooks/utils/index.ts
+++ src/hooks/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as useBoolean } from "./useBoolean";
 export { default as useAsync } from "./useAsync";
-export { AsyncStatus } from "./useAsync";
+export { default as AsyncStatus } from "./useAsync";
 export { default as useTimeSince } from "./useTimeSince";
+export { default as useCopyToClipboard } from "./useCopyToClipboard";
