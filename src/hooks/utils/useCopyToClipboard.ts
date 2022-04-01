4e396a94aff7840f66bfeab18d73a0d94bd3afa4
Nolann Biron <biron.nolann@gmail.com>
2022-04-01 13:59:11 +0200

    prettier

--- src/hooks/utils/useCopyToClipboard.ts
+++ src/hooks/utils/useCopyToClipboard.ts
@@ -6,33 +6,33 @@ type SetCopy = (text: string) => void;
 const useCopyToClipboard = (timeout = 1000): [Copied, SetCopy] => {
   const [isCopied, setIsCopied] = React.useState<Copied>(false);
 
-    const copy: SetCopy = useCallback((text: string) => {
-        if (!navigator?.clipboard) {
-            console.warn("Clipboard not supported");
-            setIsCopied(false);
-            return;
-        }   
+  const copy: SetCopy = useCallback((text: string) => {
+    if (!navigator?.clipboard) {
+      console.warn("Clipboard not supported");
+      setIsCopied(false);
+      return;
+    }
 
-        try {
-            navigator.clipboard.writeText(text);
-            setIsCopied(true);
-        } catch (err: any) {
-            setIsCopied(false);            
-        }
+    try {
+      navigator.clipboard.writeText(text);
+      setIsCopied(true);
+    } catch (err: any) {
+      setIsCopied(false);
+    }
   }, []);
 
   useEffect(() => {
     if (isCopied) {
-        const hide = setTimeout(() => {
-            setIsCopied(false)
-        }, timeout)
+      const hide = setTimeout(() => {
+        setIsCopied(false);
+      }, timeout);
 
-        return () => {
-            clearTimeout(hide)
-        }
+      return () => {
+        clearTimeout(hide);
+      };
     }
-    return undefined
-  }, [isCopied, setIsCopied, timeout])
+    return undefined;
+  }, [isCopied, setIsCopied, timeout]);
 
   return [isCopied, copy];
 };
