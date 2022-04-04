97826e6f6ef37a6a981f4a7aae9b4a2c31719d62
NolannBiron <biron.nolann@gmail.com>
2022-04-04 18:05:06 +0200

    update dist

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -518,6 +518,44 @@ const useRefresh = () => {
     return { fastRefresh: fast, slowRefresh: slow };
 };
 
+/**
+ * Use this hook when you want to animate something when it appears on the screen (e.g. when some prop set to true)
+ * but when its not on the screen you want it to be fully unmounted and not just hidden or height 0.
+ * This is especially useful when you have a table of 100s rows and each row has expandable element that appears on click.
+ * If you just set the expanding animation while keeping inactive elements mounted all those 100 elements will load the DOM,
+ * and if they all receive updates via props you're looking at 100 DOM updates for hidden elements.
+ * This hook "shows" element immediately when the isMounted is true
+ * but keeps element mounted for delayTime to let unmounting animation happen, after which you unmount element completely.
+ * delayTime should be the same as animation time in most cases.
+ */
+const useDelayedUnmount = (isMounted, delayTime) => {
+    const [shouldRender, setShouldRender] = React.useState(false);
+    React.useEffect(() => {
+        let timeoutId;
+        if (isMounted && !shouldRender) {
+            setShouldRender(true);
+        }
+        else if (!isMounted && shouldRender) {
+            timeoutId = setTimeout(() => setShouldRender(false), delayTime);
+        }
+        return () => clearTimeout(timeoutId);
+    }, [isMounted, delayTime, shouldRender]);
+    return shouldRender;
+};
+
+/**
+ * A helper hook to keep track of the time between events
+ * Can also be used to force an effect to re-run
+ */
+const useLastUpdated = () => {
+    const [lastUpdated, setStateLastUpdated] = React.useState(Date.now());
+    const previousLastUpdated = usePrevious(lastUpdated);
+    const setLastUpdated = React.useCallback(() => {
+        setStateLastUpdated(Date.now());
+    }, [setStateLastUpdated]);
+    return { lastUpdated, previousLastUpdated, setLastUpdated };
+};
+
 const UseHooksContext = React__default["default"].createContext({});
 const UseHooksProvider = ({ children, config, }) => {
     return (React__default["default"].createElement(UseHooksContext.Provider, { value: {} },
@@ -531,6 +569,7 @@ exports.useAsync = useAsync;
 exports.useBoolean = useBoolean;
 exports.useCopyToClipboard = useCopyToClipboard;
 exports.useDebounce = useDebounce;
+exports.useDelayedUnmount = useDelayedUnmount;
 exports.useElementSize = useElementSize;
 exports.useEventListener = useEventListener;
 exports.useHover = useHover;
@@ -538,6 +577,7 @@ exports.useImageLoader = useImageLoader;
 exports.useInterval = useInterval;
 exports.useIsWindowVisible = useIsWindowVisible;
 exports.useLast = useLast;
+exports.useLastUpdated = useLastUpdated;
 exports.useLocalStorage = useLocalStorage;
 exports.useLockedBody = useLockedBody;
 exports.useOnClickOutside = useOnClickOutside;
